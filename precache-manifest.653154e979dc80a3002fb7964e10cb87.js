af4990d86196c5a4584c2bc8ebc7660824de57ac
thefriction <adityaaswal98@gmail.com>
2020-08-24 23:57:22 +0530

    Updates

---
+++ precache-manifest.653154e979dc80a3002fb7964e10cb87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f867a188064a96676ecc",
+    "url": "/me/static/css/main.a6611a89.chunk.css"
+  },
+  {
+    "revision": "f867a188064a96676ecc",
+    "url": "/me/static/js/main.f867a188.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "f32cbedbd1a8ee4bf37612f6e05eab39",
+    "url": "/me/index.html"
+  }
+];
